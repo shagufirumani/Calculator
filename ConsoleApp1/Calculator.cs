b1c190f3179205a642d1f4161a9572a8a57cd362
shagufirumani <rshagufi@gmail.com>
2021-03-22 16:18:29 +0530

    Add project files.

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculator
+    {
+        int X;
+        int Y;
+        int result;
+        public void add()
+        {
+            Console.WriteLine("Enter First number");
+            X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            Y= int.Parse(Console.ReadLine());
+            result = X + Y;
+                Console.WriteLine("Addition:" + result);
+        }
+    }
+}
