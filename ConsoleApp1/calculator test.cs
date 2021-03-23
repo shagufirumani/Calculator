656a6981051ab763488a2374394b12bcab6dcf14
shagufirumani <rshagufi@gmail.com>
2021-03-23 09:31:30 +0530

    commit by shagufi (new test class)

---
+++ ConsoleApp1/calculator test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class calculator_test
+    {
+        public void test()
+        {
+            Console.WriteLine("shagufi new method");
+        }
+    }
+}
